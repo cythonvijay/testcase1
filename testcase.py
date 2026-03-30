b5c98f55f1cd23d35a9069f974c231e5e81eee15
vijay <vijaynisha343@gmail.com>
2026-03-30 15:15:58 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -3,7 +3,7 @@ numbers = [10, 20, 30, 40, 50, 60]
 total = sum(numbers)
 count = len(numbers)
 
-average = total / count:
+average = total / count;
 
 printf("Numbers:", numbers)
 print("Total:", total)
